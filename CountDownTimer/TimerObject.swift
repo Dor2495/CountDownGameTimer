08fdb83f5cbb78460ea5aa5afbd70b448d977d43
Dor Mizrachi <Dor2495@gmail.com>
2025-04-06 12:46:49 +0300

    FEATURES ADDED: - Play sound when pressing the timer buttons (to pass the turn). - Now plauer able to pass the turn on new game with no time running (to let the other player start).

---
+++ CountDownTimer/TimerObject.swift
@@ -0,0 +1,90 @@
+//
+//  ViewModel.swift
+//  CountDownTimer
+//
+//  Created by Dor Mizrachi on 03/04/2025.
+//
+
+import Foundation
+import SwiftUI
+
+@Observable
+class TimerObject {
+    // specify color?
+    var color: Color?
+    
+    var totalGameLength: Int = 0
+    // lenght
+    var length: Int
+    
+    // init
+    init(color: Color? = nil, lenght: Int) {
+        self.color = color
+        self.length = lenght
+    }
+    
+    var isTimerRunning: Bool = false
+    
+    // timeElapsed - how match time timer counted
+    var timeElapsed = 0
+    
+    // time remaining = lengh - timeElapsed
+    var remainingTime: Int {
+        length - timeElapsed
+    }
+    
+    // Timer instance?
+    var timer: Timer? = nil
+    // user the timer to set intervals and increase timeElapsed
+    
+    
+    // startTimer
+    func startTimer() {
+        isTimerRunning = true
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {[self] _ in
+            if remainingTime > 0 {
+                timeElapsed += 1
+            } else {
+                stopTimer()
+            }
+        }
+    }
+    
+    // stopTimer
+    func stopTimer() {
+        if isTimerRunning {
+            isTimerRunning = false
+            timer?.invalidate()
+        }
+    }
+    
+    // resetTimer
+    func resetTimer() {
+        timeElapsed = 0
+        isTimerRunning = false
+    }
+    
+    // progress = (lenght - timeRemaining) / lenght
+    var progress: CGFloat {
+        CGFloat(length - remainingTime) / CGFloat(length)
+    }
+    
+    var playButtonDisabled: Bool {
+        guard remainingTime > 0, !isTimerRunning else { return true}
+        return false
+    }
+    
+    var pauseButtonDisabled: Bool {
+        guard remainingTime > 0, isTimerRunning else { return true }
+        return false
+    }
+    
+    var resetButtonDisabled: Bool {
+        guard remainingTime != length, !isTimerRunning else { return true }
+        return false
+    }
+    
+    var displayTimeRemaining: String {
+        return String(format: "%02d:%02d", remainingTime / 60, remainingTime % 60)
+    }
+}
